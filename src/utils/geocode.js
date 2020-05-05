ab36c2a29ee565591c2ec0856727d711ed1fb8f2
Mitesh Pokar <49711004+mpokar@users.noreply.github.com>
2020-05-05 15:48:09 +0530

    Update geocode.js

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ address +'.json?access_token=pk.eyJ1IjoibWl0ZXNocG9rYXIiLCJhIjoiY2s5NDdvaWQxMDhwNDNtbzExb2h5dGI5ZSJ9.fbd-OCHhoSjOlgnyBbOvIQ&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ address +'.json?access_token=ENTER_API_KEY&limit=1'
 
     request({url, json: true}, (error, {body}=[]) =>{
 
