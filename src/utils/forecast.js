99f634988053a9af6880dcf58af3cd6b812866a4
Mitesh Pokar <49711004+mpokar@users.noreply.github.com>
2020-05-05 15:47:22 +0530

    Update forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 const forecast = (address, callback) =>{
 
-    const url = 'https://api.openweathermap.org/data/2.5/find?q='+ address +'&appid=00b029e1976263ac665d25e2ee6a0685&units=metric'
+    const url = 'https://api.openweathermap.org/data/2.5/find?q='+ address +'&appid=ENTER_API_KEY&units=metric'
     request({url, json: true}, (error, {body}) => {
         if (error){
           callback('Unable to connect to weather serivce', undefined)
